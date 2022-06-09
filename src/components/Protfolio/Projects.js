e90c5c57271976f13e12729771b210f97e8e81d7
163002029.junayed <junayednoor@gmail.com>
2022-06-09 22:44:20 +0600

    projects page

--- src/components/Protfolio/Projects.js
+++ src/components/Protfolio/Projects.js
@@ -1,43 +1,184 @@
 import React from "react";
-import t from "../../assets/trust.png";
-import c from "../../assets/car.png";
-import i from "../../assets/immi.png";
-import tr from "../../assets/trust.gif";
-import ca from "../../assets/car.gif";
-import im from "../../assets/immi.gif";
+import t from "../../assets/trust.gif";
+import c from "../../assets/car.gif";
+import i from "../../assets/immi.gif";
 const Projects = () => {
   return (
     <div>
       <section>
-        <div class="stats bg-primary text-primary-content h-screen">
+        <div class="stats bg-primary text-primary-content max-w-3xl mx-auto block my-6">
           <div class="stat">
-            <div class="stat-title">Account balance</div>
-            <div class="stat-value">$89,400</div>
-            <div class="stat-actions">
-              <button class="btn btn-sm btn-success">Add funds</button>
-            </div>
-          </div>
-
-          <div class="stat">
-            <div class="stat-title">Current balance</div>
-            <div class="stat-value">$89,400</div>
-            <div class="stat-actions">
-              <button class="btn btn-sm">Withdrawal</button>
-              <button class="btn btn-sm">deposit</button>
-            </div>
+            <div class="stat-value text-center">My Projects</div>
           </div>
         </div>
       </section>
       <section>
-        <div class="grid grid-rows-3 grid-flow-col gap-4 h-80">
-          <div class="row-start-1 row-span-2">
-            <img src={t} alt="" />
+        <div class="grid grid-cols-1   gap-4 ">
+          <div class="">
+            <div className="max-w-3xl mx-auto my-3">
+              <h1 className="text-3xl font-bold">
+                Trust-Accessories{" "}
+                <span className="text-lg text-secondary">(manufacture management) </span>
+              </h1>
+              <div className="flex gap-3">
+                <span
+                  onClick={() => window.open("https://trust-accessories.web.app/")}
+                  className="my-5  text-lg  flex hover:text-blue-500"
+                  role="button"
+                >
+                  <div className="mr-2 underline">Live Site</div>
+                </span>
+                <span
+                  onClick={() =>
+                    window.open("https://github.com/JunayedKhanNoor/Trust-Accessories-Client")
+                  }
+                  className="my-5  text-lg  flex hover:text-blue-500"
+                  role="button"
+                >
+                  <div className="mr-2 underline">Client Side Code</div>
+                </span>
+                <span
+                  onClick={() =>
+                    window.open("https://github.com/JunayedKhanNoor/Trust_Accessories-Server")
+                  }
+                  className="my-5  text-lg  flex hover:text-blue-500"
+                  role="button"
+                >
+                  <div className="mr-2 underline">Server Side Code</div>
+                </span>
+              </div>
+              <h1 className="text-3xl font-bold text-secondary">Description: </h1>
+              <ul className="list-disc my-3">
+                <li>
+                  This application is a manufacturer management application for tools manufacturer.
+                </li>
+                <li>
+                  Where tools manufacturer can upload their products and update and manage their
+                  stokes.
+                </li>
+                <li>
+                  They can limit minimum purchase quantity for vendors. Dashboard designed for admin
+                  and user.
+                </li>
+              </ul>
+              <h1 className="text-3xl font-bold text-secondary my-3">Technologies: </h1>
+              <h3 className="text-md font-bold  my-3">
+                HTML, CSS, Tailwind CSS, React, Firebase Authentication, Express, MongoDB, Heroku,
+                React Hook Form, React Query, Stripe
+              </h3>
+            </div>
+
+            <img className="max-w-3xl mx-auto" src={t} alt="" />
           </div>
-          <div class="row-start-2 row-span-2">
-            <img src={c} alt="" />
+          <div class="">
+            <div className="max-w-3xl mx-auto my-3">
+              <h1 className="text-3xl font-bold">
+                Trust-Accessories{" "}
+                <span className="text-lg text-secondary">(manufacture management) </span>
+              </h1>
+              <div className="flex gap-3">
+                <span
+                  onClick={() => window.open("https://trust-accessories.web.app/")}
+                  className="my-5  text-lg  flex hover:text-blue-500"
+                  role="button"
+                >
+                  <div className="mr-2 underline">Live Site</div>
+                </span>
+                <span
+                  onClick={() =>
+                    window.open("https://github.com/JunayedKhanNoor/Trust-Accessories-Client")
+                  }
+                  className="my-5  text-lg  flex hover:text-blue-500"
+                  role="button"
+                >
+                  <div className="mr-2 underline">Client Side Code</div>
+                </span>
+                <span
+                  onClick={() =>
+                    window.open("https://github.com/JunayedKhanNoor/Trust_Accessories-Server")
+                  }
+                  className="my-5  text-lg  flex hover:text-blue-500"
+                  role="button"
+                >
+                  <div className="mr-2 underline">Server Side Code</div>
+                </span>
+              </div>
+              <h1 className="text-3xl font-bold text-secondary">Description: </h1>
+              <ul className="list-disc my-3">
+                <li>
+                  This application is a manufacturer management application for tools manufacturer.
+                </li>
+                <li>
+                  Where tools manufacturer can upload their products and update and manage their
+                  stokes.
+                </li>
+                <li>
+                  They can limit minimum purchase quantity for vendors. Dashboard designed for admin
+                  and user.
+                </li>
+              </ul>
+              <h1 className="text-3xl font-bold text-secondary my-3">Technologies: </h1>
+              <h3 className="text-md font-bold  my-3">
+                HTML, CSS, Tailwind CSS, React, Firebase Authentication, Express, MongoDB, Heroku,
+                React Hook Form, React Query, Stripe
+              </h3>
+            </div>
+            <img className="max-w-3xl mx-auto" src={c} alt="" />
           </div>
-          <div class="row-start-1 row-span-2">
-            <img src={i} alt="" />
+          <div class="">
+            <div className="max-w-3xl mx-auto my-3">
+              <h1 className="text-3xl font-bold">
+                Trust-Accessories{" "}
+                <span className="text-lg text-secondary">(manufacture management) </span>
+              </h1>
+              <div className="flex gap-3">
+                <span
+                  onClick={() => window.open("https://trust-accessories.web.app/")}
+                  className="my-5  text-lg  flex hover:text-blue-500"
+                  role="button"
+                >
+                  <div className="mr-2 underline">Live Site</div>
+                </span>
+                <span
+                  onClick={() =>
+                    window.open("https://github.com/JunayedKhanNoor/Trust-Accessories-Client")
+                  }
+                  className="my-5  text-lg  flex hover:text-blue-500"
+                  role="button"
+                >
+                  <div className="mr-2 underline">Client Side Code</div>
+                </span>
+                <span
+                  onClick={() =>
+                    window.open("https://github.com/JunayedKhanNoor/Trust_Accessories-Server")
+                  }
+                  className="my-5  text-lg  flex hover:text-blue-500"
+                  role="button"
+                >
+                  <div className="mr-2 underline">Server Side Code</div>
+                </span>
+              </div>
+              <h1 className="text-3xl font-bold text-secondary">Description: </h1>
+              <ul className="list-disc my-3">
+                <li>
+                  This application is a manufacturer management application for tools manufacturer.
+                </li>
+                <li>
+                  Where tools manufacturer can upload their products and update and manage their
+                  stokes.
+                </li>
+                <li>
+                  They can limit minimum purchase quantity for vendors. Dashboard designed for admin
+                  and user.
+                </li>
+              </ul>
+              <h1 className="text-3xl font-bold text-secondary my-3">Technologies: </h1>
+              <h3 className="text-md font-bold  my-3">
+                HTML, CSS, Tailwind CSS, React, Firebase Authentication, Express, MongoDB, Heroku,
+                React Hook Form, React Query, Stripe
+              </h3>
+            </div>
+            <img className="max-w-3xl mx-auto" src={i} alt="" />
           </div>
         </div>
       </section>
