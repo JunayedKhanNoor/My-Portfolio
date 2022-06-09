50b69d1720da3eab983b95f7741f56ee3365a896
163002029.junayed <junayednoor@gmail.com>
2022-06-09 19:18:29 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#4f46e5",
+          secondary: "#6366f1",
+          accent: "#ef4444",
+          neutral: "#3d4451",
+          info: "#19D3AE",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
