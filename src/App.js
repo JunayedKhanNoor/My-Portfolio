1277b8de665368e2f1d84593c0ff821e85638415
163002029.junayed <junayednoor@gmail.com>
2022-06-09 21:23:47 +0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "react-toastify/dist/ReactToastify.css";
 import MyPortfolio from './components/Protfolio/MyPortfolio';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
+import Navbar from './components/Protfolio/Navbar';
 
 function App() {
   AOS.init();
   return (
     <div >
+      <Navbar></Navbar>
       <MyPortfolio></MyPortfolio>
       <ToastContainer />
     </div>
