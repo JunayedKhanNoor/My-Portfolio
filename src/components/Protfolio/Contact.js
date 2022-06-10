fcce467523850c0b973d4f281a5de90c671c638e
163002029.junayed <junayednoor@gmail.com>
2022-06-10 11:57:25 +0600

    Email sent

--- src/components/Protfolio/Contact.js
+++ src/components/Protfolio/Contact.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import { toast } from "react-toastify";
 import me from "../../assets/Junayed.jpg";
 import Typical from "react-typical";
+import emailjs from "@emailjs/browser";
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_j8kn29d", "template_v5i5kg8", form.current, "tMOELrBbtkOHfsHCu").then(
+      (result) => {
+        console.log(result.text);
+        toast.success("Thanks for email!");
+      },
+      (error) => {
+        console.log(error.text);
+        toast.error("Send failed!");
+      }
+    );
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e.target.name.value);
@@ -16,7 +32,7 @@ const Contact = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <img src={me} alt="" className="rounded-full h-60 w-60 border-4" />
-          <div className="text-5xl font-bold">
+          <div className="text-5xl font-bold text-primary">
             <Typical
               steps={["Contact With Junayed!", 1000, "JavaScript Developer", 1000]}
               loop={Infinity}
@@ -30,7 +46,7 @@ const Contact = () => {
           <p className="py-2">Call: +8801303021131</p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleSubmit}>
+          {/* <form onSubmit={handleSubmit}>
             <div className="card-body">
               <div className="form-control">
                 <label className="label">
@@ -72,6 +88,26 @@ const Contact = () => {
                   Send Email
                 </button>
               </div>
+            </div>
+          </form> */}
+          <form ref={form} onSubmit={sendEmail}>
+            <div className="card-body">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              {/* <label>Name</label> */}
+              <input type="text" name="user_name" className="input input-bordered"/>
+              {/* <label>Email</label> */}
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="email" name="user_email" className="input input-bordered"/>
+              {/* <label>Message</label> */}
+              <label className="label">
+                <span className="label-text">Message</span>
+              </label>
+              <textarea name="message" className="textarea textarea-bordered" />
+              <input type="submit" value="Send" className="btn btn-primary" />
             </div>
           </form>
         </div>
