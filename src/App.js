4a9170937e01b3e789985c00a73b5d662be6fb5c
163002029.junayed <junayednoor@gmail.com>
2022-06-12 15:43:22 +0600

    404 route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import AutoMart from './components/Protfolio/AutoMart';
 import TrustAccessories from './components/Protfolio/TrustAccessories';
 import Blogs from './components/Blogs';
 import AboutMe from './components/Protfolio/AboutMe';
+import NotFound from './components/NotFound';
 
 function App() {
   AOS.init();
@@ -29,6 +30,7 @@ function App() {
       <Route path='/blogs' element={<Blogs></Blogs>}></Route>
       <Route path='/about' element={<AboutMe></AboutMe>}></Route>
       <Route path='/immigration' element={<ImmigrationLab></ImmigrationLab>}></Route>
+      <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <ToastContainer />
     </div>
